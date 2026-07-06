20dd1af0cab1d54cdf6d007221ebfed332dd516e
Pedro Carneiro <pedro.aco@ufop.aluno.edu.br>
2026-07-06 20:45:31 -0300

    Adiciona model Categoria com relacionamento 1:N (Parte 3)
    Implementa o roteiro-django-projeto-parte-3: model Categoria, ForeignKey em
    Mensagem (SET_NULL, related_name=mensagens), registro no admin com filtro
    por categoria, migration aplicada e selo de categoria exibido no template.

--- home/admin.py
+++ home/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
-from .models import Mensagem
+from .models import Categoria, Mensagem
+
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ("nome",)
+    search_fields = ("nome",)
 
 
 @admin.register(Mensagem)
 class MensagemAdmin(admin.ModelAdmin):
-    list_display = ("titulo", "criada_em")
+    list_display = ("titulo", "categoria", "criada_em")
+    list_filter = ("categoria",)
     search_fields = ("titulo", "conteudo")
