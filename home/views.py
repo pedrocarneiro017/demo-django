67f80b1756e0f9bb41fcdfe82ddca066d3d4e24d
Pedro Carneiro <pedro.aco@ufop.aluno.edu.br>
2026-07-06 20:23:32 -0300

    Adiciona campo autor, página sobre e novo visual (Parte 2)
    Implementa o roteiro-django-projeto-parte-2: campo autor no model Mensagem
    com migration, exibição do autor no template, nova página /sobre/ (view,
    rota e template) linkada no menu, e troca do gradiente da home.

--- home/views.py
+++ home/views.py
@@ -6,3 +6,7 @@ from .models import Mensagem
 def index(request):
     mensagens = Mensagem.objects.all()
     return render(request, "home/index.html", {"mensagens": mensagens})
+
+
+def sobre(request):
+    return render(request, "home/sobre.html")
